bfa1b1692553f1da9e67d783eb799ab055da5918
albertostizzoli <albertostizzoli@yahoo.it>
2023-10-18 15:30:37 +0200

    Create Function for Square

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// creo una variabile e un evento per il bottone
+// creo una variabile  per il bottone
 const btn = document.querySelector('button');
 
 btn.addEventListener('click', function(){
@@ -6,9 +6,26 @@ btn.addEventListener('click', function(){
     const grid = document.getElementById('grid');
     grid.innerHTML = '';
     for(let i = 0; i < numSquare; i++){
+        let square = drawSquare(i, numSquare);
         console.log(square);
         grid.append(square);
     }
 });
+
+// Creo una funzione che disegni i quadratini
+ function drawSquare(squareIndex,numSquare){
+    const squareWidth = Math.sqrt(numSquare);
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.style.width = `calc(100% / ${squareWidth})`;
+    square.style.width = square.style.width;
+    square.innerHTML = squareIndex + 1;
+    square.addEventListener('click', function(){
+        square.classList.add('active');
+        square.style.color = 'black';
+
+    });
+      return square;
+ }
 
 
